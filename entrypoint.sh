78bd1788530824dbbfa0c353e0a9467b25065fc2
Adam Breaux <adam.breaux@gopuff.com>
2019-11-26 12:46:09 -0500

    Mark executable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 HAPROXY_BIN=/usr/sbin/haproxy
 HAPROXY_CFG=/etc/haproxy/haproxy.cfg
+HAPROXY_FLAGS="-V"
+
+if [ -f ${HAPROXY_CFG} ]; then
+  echo "Starting HAProxy..."
+  ${HAPROXY_BIN} ${HAPROXY_FLAGS} -f ${HAPROXY_CFG}
+fi
